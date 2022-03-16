7c28c4b3f9cf3691b7d7dd206983aa20ccd9d571
ferentsalex <Alex@lumen.me>
2022-03-16 11:04:14 +0200

    Exercise

--- lib/blocs/workout_search_bloc.dart
+++ lib/blocs/workout_search_bloc.dart
@@ -26,30 +26,14 @@ class WorkoutSearchBloc extends Bloc<SearchEvent, ResultState> {
   final WorkoutsService workoutsService;
 
   WorkoutSearchBloc(this.workoutsService) : super(const ResultState(<dynamic>[])) {
-    on<InitEvent>((event, emit) => _initState(emit));
-    on<SearchWorkoutEvent>((event, emit) => _onSearchState(event.query, emit));
-    add(InitEvent());
+
   }
 
   void _onSearchState(String query, Emitter<ResultState> emit) {
-    final List<dynamic> filteredList = <dynamic>[];
-    if (query.isNotEmpty) {
-      filteredList.clear();
-      for (var item in items) {
-        if (item['name'].contains(query)) {
-          filteredList.add(item);
-        }
-      }
-      emit(ResultState(filteredList));
-    } else {
-      filteredList.clear();
-      filteredList.addAll(items);
-      emit(ResultState(filteredList));
-    }
+
   }
 
   Future<void> _initState(Emitter<ResultState> emit) async {
     items = await workoutsService.loadWorkouts();
-    emit(ResultState(items));
   }
 }
--- lib/widgets/workout_search_list.dart
+++ lib/widgets/workout_search_list.dart
@@ -1,48 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:office_pair_programming/blocs/workout_search_bloc.dart';
+
 
 class WorkoutSearchList extends StatelessWidget {
   WorkoutSearchList({Key? key}) : super(key: key);
-  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            onChanged: (value) {
-              context.read<WorkoutSearchBloc>().add(SearchWorkoutEvent(value));
-            },
-            controller: _controller,
-            decoration: const InputDecoration(
-                labelText: "Search",
-                hintText: "Search",
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-          ),
-        ),
-        Expanded(
-          child: BlocBuilder<WorkoutSearchBloc, ResultState>(
-              builder: (context, state) {
-            return ListView.builder(
-              itemCount: state.result.length,
-              itemBuilder: (context, index) {
-                final item = state.result[index];
-                return ListTile(
-                  title: Text(item['name']),
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    backgroundImage: AssetImage(item['image']),
-                  ),
-                );
-              },
-            );
-          }),
-        ),
-      ],
-    );
+    return const Text("Display the list here");
   }
 }
